63f2980310feb1eae7bd934a311067d03d25261e
Hadar Abukrat <hadarabukrat@Chaim-As-MacBook.local>
2018-06-27 00:06:56 -0700

    taking notes

---
+++ arduino.cpp
@@ -0,0 +1,18 @@
+//c++ code for aduino
+
+
+// this funtion ony runs once when the arduino turns on
+void setup () {
+
+}
+
+//this method runs continuously afterwards
+void loop() {
+
+	//we will be using output for the LED lights
+	//we will be using input for the buttons
+	pinMode(pin, INPUT/OUTPUT); //this is an example of setting a pin to input or output
+	digitalWrite(pin, HIGH/LOW); //example of setting pin voltage to high(5V) or low(ground) if output
+								//in input, set high(enable) or low(disable)
+
+}
