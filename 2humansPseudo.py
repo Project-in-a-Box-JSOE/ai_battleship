4eeb6f4faa230b6f88407162752dbc8dd73b8497
Hadar Abukrat <hadarabukrat@Chaim-As-MacBook.local>
2018-08-21 17:26:04 -0700

    updated challenge 2 psuedocode

--- 2humansPseudo.py
+++ 2humansPseudo.py
@@ -11,6 +11,7 @@ class Ship:
 # --------------------- FUNCTIONS THAT MAY BE USEFUL ----------------------
 # These functions will still be helpful once you implement that AI
 # But they may need to be updated later on
+# You may also create ay additional functions you find useful
 
 
 # Name: getHumanShipInput()
@@ -40,6 +41,21 @@ def humanTurn(playerTopMatrix):
 	# Return inputted locations
 
 
+# Name: updateBoard()
+# Description: In this function, we update the playerTopMatrix with every move,
+# and we update ship objects in shipMatrix to detect sinks in the future
+# Input: X/Row location, Y/Column location, playerTopMatrix(matrix of players moves),
+# 		otherPlayerBottomMatrix(other players matrix to check if ship is there)
+# 		otherPlayerShips(contains opponents ships)
+# Output: None
+def updateBoard(x, y, playerTopMatrix, otherPlayerBottomMatrix, otherPlayerShips):
+	# TODO
+	# Check the other players matrix if ship is there
+		#update playerTopMatrix accordingly
+	# If ship was hit, update ship object with hit and/or hit location
+		# Only if you choose to implement the game this way
+
+
 # Name: isGameOver()
 # Description: This function checks to see if the game is over. Called after
 # each human or Ai turn
@@ -48,6 +64,8 @@ def humanTurn(playerTopMatrix):
 #         False - game is not over because not all ships have been sunk
 def isGameOver(shipsList):
 	# TODO
+	# Loop through list of ships
+		# if all ships have sunk, game is over
 
 
 # ------------------------------ MAIN CODE -------------------------------
@@ -68,10 +86,10 @@ player2Ships = getHumanShipInput(player2BottomMatrix)
 While game is not over
 	# First players turn
 	humanTurn(player1TopMatrix)
-	updateBoards()
-	isGameOver()
+	updateBoard()
+	isGameOver(player2Ships)
 
 	# Second players turn
 	humanTurn(player2TopMatrix)
-	updateBoards()
-	isGameOver()
+	updateBoard()
+	isGameOver(player1Ships)
